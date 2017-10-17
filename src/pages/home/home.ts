c16b89ec78c6848a43e84927ff1bb109bd0c5244
Leonardo Salles <leosallestm@gmail.com>
2017-10-16 22:39:31 -0200

    adjust examples

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,10 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  showControls: boolean;
+  showControls: boolean = true;
+  scale: number = 1;
 
   constructor(public navCtrl: NavController) {
-    this.showControls = true;
   }
 
+  afterZoomIn (event) {
+    console.log('After ZoomIn Event: ', event);
+  }
+
+  afterZoomOut (event) {
+    console.log('After ZoomOut Event: ', event);
+  }
 }
